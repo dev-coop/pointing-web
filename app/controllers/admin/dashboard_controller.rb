44d43dcce52fd6c359da1e88baa48162be9fd4fe
Jason Rutherford <jason.rutherford@plansource.com>
2015-04-25 13:11:55 -0700

    #93268950 dashboard created

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,10 @@
+class Admin::DashboardController < ApplicationController
+  # before_filter :authenticate_user!
+
+  # GET /admin/dashboard
+  # GET /admin/dashboard.json
+  def index
+    
+  end
+
+end
---
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class Admin::DashboardControllerTest < ActionController::TestCase
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+end
