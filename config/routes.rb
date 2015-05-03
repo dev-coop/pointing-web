740244903cfa0f29d3fb2d46a8fc5cefdb68e0f0
Jason Rutherford <jason.rutherford@plansource.com>
2015-05-03 14:51:46 -0700

    #92191822 initial API. Versioned by route path.  Five random locations are returned as json.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
     resources :locations
   end
   devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout' }
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :locations
+    end
+  end
+
   root to: "admin/dashboard#index"
 end
