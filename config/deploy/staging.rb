f775b8d8d1099b3f67f9a00c6a8b8be1456c9498
Jason Rutherford <jason.rutherford@plansource.com>
2015-05-04 13:13:14 -0700

    #93778778 deploys to staging will use the develop git branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,4 +6,4 @@ set :rvm_ruby_version, '2.2.2@getpointing-staging'
 
 set :ssh_options, { forward_agent: true }
 set :deploy_to, '/home/rails/staging'
-set :branch, 'feature/deployment'
+set :branch, 'develop'
