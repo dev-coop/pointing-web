31ff9b763143533763ca481b6774023b80991764
Jason Rutherford <jason.rutherford@plansource.com>
2015-05-04 11:07:39 -0700

    #93778778 rename deploy stage from test to staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,9 @@
-role :app, %w{deploy@getpointing.com}
-role :web, %w{deploy@getpointing.com}
-role :db,  %w{deploy@getpointing.com}
+role :app, %w{deploy@192.241.220.95}
+role :web, %w{deploy@192.241.220.95}
+role :db,  %w{deploy@192.241.220.95}
 
 set :rvm_ruby_version, '2.2.2@getpointing-prod'
 
 set :ssh_options, { forward_agent: true }
 set :deploy_to, '/home/rails/prod'
+set :branch, 'feature/deployment'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,8 @@ role :app, %w{deploy@192.241.220.95}
 role :web, %w{deploy@192.241.220.95}
 role :db,  %w{deploy@192.241.220.95}
 
-set :rvm_ruby_version, '2.2.2@getpointing-test'
+set :rvm_ruby_version, '2.2.2@getpointing-staging'
 
 set :ssh_options, { forward_agent: true }
-set :deploy_to, '/home/rails/test'
+set :deploy_to, '/home/rails/staging'
 set :branch, 'feature/deployment'
