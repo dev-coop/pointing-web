8862ff9bb787fef0b148818d19b90aaf5014bab2
Jason Rutherford <jason.rutherford@plansource.com>
2015-05-03 20:58:38 -0700

    #92191822 improved api routing paths. Adds subdomain support with pretty api paths.  Added some documentation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,30 @@
 Rails.application.routes.draw do
+
+  #
+  # API Routes
+  #
+  # Rather than constraining to subdomain let's match domain beginning with 'api.' so that we
+  # can access 'http://api.localhost:3000/' which is closer to the rails convention of using
+  # http://localhost:3000.  To get is working you just need to add this to /etc/hosts:
+  #   127.0.0.1   api.localhost
+
+  constraints domain: /^api\.*./ do
+    namespace :api, path: '', defaults: { format: :json } do
+      namespace :v1 do
+        resources :locations
+      end
+    end
+  end
+
+  #
+  # Web Admin Routes
+  #
+
   namespace :admin do
     resources :locations
   end
-  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      resources :locations
-    end
-  end
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout' }
 
   root to: "admin/dashboard#index"
 end
