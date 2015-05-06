456f2c34aec66dae474a3a1ce51e61f9bf4d1929
Jason Rutherford <jason.rutherford@plansource.com>
2015-05-05 22:38:25 -0700

    #93267636 added postgresql support

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150501163624) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
@@ -43,7 +46,7 @@ ActiveRecord::Schema.define(version: 20150501163624) do
     t.boolean  "is_admin",               default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
