8b8bd3c0c9e01819a0517c60c7e6e524a7e02249
Jason Rutherford <jason.rutherford@plansource.com>
2015-08-23 18:18:01 -0700

    WIP updating locations to support distance away.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -4,12 +4,22 @@ module Api
       respond_to :json
 
       def index
-        respond_with  Location.where('last_geocoded_at IS NOT NULL')
-                              .reorder("RANDOM()")
-                              .limit(5)
-                              .pluck(:id, :name, :address, :lat, :lng, :elevation)
+        # locations = Location.where('last_geocoded_at IS NOT NULL')
+        #   .reorder("RANDOM()")
+        #   .limit(5)
+        #   .pluck(:id, :name, :address, :lat, :lng, :elevation, :distance)
 
+
+        me = Location.new(address: 'Post Falls, Id').geocode
+
+        locations = Location.near(me)
+
+        # locations.each { |l| l << l.distance(me) }
+
+        respond_with locations
       end
+
+
     end
   end
 
